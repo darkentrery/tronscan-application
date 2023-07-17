e58a546824321df19277f65d2f81ac10bf766f93
darkentrery <badajkovmihail@gmail.com>
2023-07-17 11:03:25 +0300

    parse instruction

--- src/main/java/ru/intelinvest/tronscan/models/ParseInstruction.java
+++ src/main/java/ru/intelinvest/tronscan/models/ParseInstruction.java
@@ -1,5 +1,10 @@
 package ru.intelinvest.tronscan.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ParseInstruction {
     private boolean correctQuantityByShareLotSize;
     private boolean correctQuantityByAmountAndPrice;
